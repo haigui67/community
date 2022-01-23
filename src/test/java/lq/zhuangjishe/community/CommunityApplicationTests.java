9bee4258a52636b6e13c750da9ab99cfad606d52
haigui67 <546174905.com>
2022-01-23 11:34:31 +0800

    init repo

---
+++ src/test/java/lq/zhuangjishe/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package lq.zhuangjishe.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
